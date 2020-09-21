99a37a278d1dd88b71d480f68acbfac324ce8186
sandeeprachha <rachhasandeepkumar.4a3>
2020-09-21 16:29:43 +0530

    inital commit

---
+++ Example/Pods/Target Support Files/Pods-RScrollView_Tests/Pods-RScrollView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RScrollView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RScrollView_Tests
+@end
---
+++ Example/Pods/Target Support Files/RScrollView/RScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RScrollView : NSObject
+@end
+@implementation PodsDummy_RScrollView
+@end
